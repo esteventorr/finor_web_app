0903511e7ddef33e16245f2fcd9dc05278816438
Brahiam Quintero <brahiamquintero@hotmail.com>
2023-03-19 16:58:04 -0500

    [BE-01] POC Clean Up

--- finor_web_app/urls.py
+++ finor_web_app/urls.py
@@ -14,12 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from sandbox import views as sandboxViews
+from django.urls import path, include 
 
 urlpatterns = [
-    path("__reload__/", include("django_browser_reload.urls")),
-    path('', sandboxViews.saved_expenses),
-    path('get_expenses/', sandboxViews.get_expenses, name="get_expenses"),
+    path("__reload__/", include("django_browser_reload.urls")),  
     path('admin/', admin.site.urls),
 ]
