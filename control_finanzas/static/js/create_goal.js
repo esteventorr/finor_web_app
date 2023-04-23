0b1e9f2c2fdb8e7594b665d3460536c58989d2e3
bened18 <belt@eafit.edu.co>
2023-04-23 13:20:18 -0500

    resolved problems with parsing the expenses when posting them

--- control_finanzas/static/js/create_goal.js
+++ control_finanzas/static/js/create_goal.js
@@ -1,11 +1,13 @@
 $("#goalForm").on("submit", function (event) {
   event.preventDefault();
 
-  // Recoge los datos del formulario
+  const valueInput = $("#value-input");
+  const floatValue = unformatCurrency(valueInput.val());
+  valueInput.val(floatValue);
+
   const formData = new FormData(this);
   const formURL = $(this).data("url");
 
-  // Realiza la solicitud AJAX
   $.ajax({
     url: formURL,
     type: "POST",
@@ -13,15 +15,12 @@ $("#goalForm").on("submit", function (event) {
     processData: false,
     contentType: false,
     success: function (response) {
-      // Muestra el mensaje de éxito
       console.warn(response);
       $("#message").html("<p>Objetivo guardado con éxito.</p>");
       $("#message").css("color", "green");
-      // Recarga la página
       location.reload();
     },
     error: function () {
-      // Muestra el mensaje de error
       $("#message").html(
         "<p>Error al guardar el objetivo. Inténtalo de nuevo.</p>"
       );
