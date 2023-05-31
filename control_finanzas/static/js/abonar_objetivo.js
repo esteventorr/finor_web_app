1d4672c39ede129cac2a95692926a17e422537c0
Brahiam E. Quintero T <brahiamquintero@hotmail.com>
2023-05-31 07:36:43 -0500

    Abono a Goals mejorado

--- control_finanzas/static/js/abonar_objetivo.js
+++ control_finanzas/static/js/abonar_objetivo.js
@@ -1,61 +1,56 @@
-const addButton = document.querySelector('.add-button');
-const modal = document.querySelector('.modal');
-const closeModalbutton = document.querySelector('.close-modal');
-const submitButton = document.querySelector('submit-button');
-const progressElement = document.querySelector('.progress');
-
-addButton.addEventListener('click', openModal);
-closeModalbutton.addEventListener('click', closeModal);
-submitButton.addEventListener('click', increaseProgress);
-
-function openModal() {
-    modal.style.display = 'block';
+function openModal(event) {
+  const goalId = $(event.target).attr("data-goal-id");
+  $(`.modal.${goalId}`).css("display", "block");
 }
 
-function closeModal() {
-    modal.style.display = 'none';
+function closeModal(event) {
+  $(event.target).closest(".modal").css("display", "none");
 }
 
-function increaseProgress() {
-    let currentProgress = parseInt(progressElement.style.width) || 0;
-    const increment = 10; // Cantidad que aumenta la barra de progreso
+function increaseProgress(event) {
+  const modal = $(event.target).closest(".modal");
+/*   let currentProgress = parseInt(progressElement.style.width) || 0;
+  const increment = 10; // Cantidad que aumenta la barra de progreso
 
-    if (currentProgress + increment <= 100) {
-        currentProgress += increment;
-        progressElement.style.width = currentProgress + '%';
-    }
-    closeModal();
+  if (currentProgress + increment <= 100) {
+    currentProgress += increment;
+    progressElement.style.width = currentProgress + "%";
+  }
+  closeModal(); */
 }
 
-$("#value-form").on("submit", function (event) {
-    event.preventDefault();
+const addButtons = $(".add-button");
+const modal = document.querySelector(".modal");
+const closeModalbutton = $(".close-modal");
+const submitButton = $(".submit-button");
+const progressElement = $(".progress");
 
-    const valueInput = $("#value-input");
-    const floatValue = unformatCurrency(valueInput.val());
-    console.log("floatValue ------------------------------------------------------------------------")
-    console.log(floatValue)
-    valueInput.val(floatValue);
+addButtons.on("click", openModal);
+closeModalbutton.on("click", closeModal);
+submitButton.on("click", increaseProgress);
 
-    const formData = new FormData(this);
-    const formURL = $(this).data("url");
+$(".value-form").on("submit", function (event) {
+  event.preventDefault();
 
-    $.ajax({
-        url: formURL,
-        type: "POST",
-        data: formData,
-        processData: false,
-        contentType: false,
-        success: function (response) {
-          console.warn(response);
-          $("#message").html("<p>Abono realizado con éxito.</p>");
-          $("#message").css("color", "green");
-          window.location.href = location.origin + "/metas-ahorros/";
-        },
-        error: function () {
-          $("#message").html(
-            "<p>Error al abonar al objetivo. Inténtalo de nuevo.</p>"
-          );
-          $("#message").css("color", "red");
-        },
-      });
-    });
+  const formData = new FormData(this);
+  const formURL = $(this).data("url");
+
+  $.ajax({
+    url: formURL,
+    type: "POST",
+    data: formData,
+    processData: false,
+    contentType: false,
+    success: function (response) {
+      console.warn(response);
+      $("#message").html("<p>Abono realizado con éxito.</p>");
+      $("#message").css("color", "green");
+      $(".modal").css("display", "none");
+      window.location.href = location.origin + "/metas-ahorros/";
+    },
+    error: function () {
+      $("#message").html("<p>Error al abonar al objetivo. Inténtalo de nuevo.</p>");
+      $("#message").css("color", "red");
+    },
+  });
+});
--- control_finanzas/views.py
+++ control_finanzas/views.py
@@ -115,16 +115,20 @@ def ingresar_gastos(request):
 @require_auth
 def ingresar_objetivos(request):
     logging.info("Ingresando objetivos...")
+    unfiltered_expenses = GET_expenses(request)
+    expenses = [expense for expense in unfiltered_expenses if expense.category == "goalabono"]
     goals = GET_goals(request)
     if goals:
         mensaje = "Objetivo creado con éxito."
     else:
         mensaje = "Error al crear el objetivo."
     logging.info(mensaje)
+    for goal in goals:
+        goal.total_expenses = sum(int(expense.value) for expense in expenses if expense.description == goal.id)
     return render(
         request,
         "control_finanzas/crear-objetivos.html",
-        {"mensaje": mensaje, "goals": goals},
+        {"mensaje": mensaje, "goals": goals, "expenses": expenses},
     )
 
 @require_auth
@@ -167,7 +171,8 @@ def analisis_gastos(request):
     user_display_name = request.session['user_display_name']
     user_uid = request.session['user_uid']
     logging.info(f"Usuario autenticado: {user_display_name} {user_uid}")
-    expenses = GET_expenses(request)
+    unfiltered_expenses = GET_expenses(request)
+    expenses = [expense for expense in unfiltered_expenses if expense.category != "goalabono"]
     rankings = []
     if expenses:
         # Agrupar gastos por categoría
@@ -337,6 +342,31 @@ def create_goal(request):
         response_data = response.json()
         logging.info(response_data)
         return JsonResponse(response_data)
+    else:
+        return JsonResponse({"error": "Invalid request method"})
+    
+@csrf_exempt
+def addgoalabonoexpense(request):
+    if request.method == "POST":
+        data = request.POST
+        logging.info(data)
+        try:
+            image_url = "https://esteventorr.github.io/images/graphical/no-image.png"
+            expense = Expense(
+                value=data["value-input"],
+                description=data["goal-id"],
+                category="goalabono",
+                photo=image_url,
+                date="2000-01-01",
+                user=request.session['user_display_name'],
+            )
+            POST_expense(
+                expense
+            )
+            return JsonResponse({"message": "Abono generado exitosamente"})
+        except Exception as e:
+            logging.error(f"Error al abonar: {e}")
+            return JsonResponse({"message": "Error al abonar"}, status=500)
     else:
         return JsonResponse({"error": "Invalid request method"})
 
